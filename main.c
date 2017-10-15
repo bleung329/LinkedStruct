28f4352bd187200b091a62a49e70502ad2d516f5
bleung329 <bleung@stuy.edu>
2017-10-15 08:12:56 -0400

    Working

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <node.h>
+#include "node.h"
 
 int main()
 {
-	
+	node* blah = (node*) calloc(1,sizeof(node));
+	blah->data = 3;
+	printf("%d\n", blah->data);
+	printf("%p\n", blah->next);
+	print_list(blah);
+	free(blah);
+	return 0;
 }
--- node.h
+++ node.h
@@ -3,6 +3,29 @@
 
 typedef struct
 {
-	struct node* tail;
-	int* data;
+	int data;
+	struct node* next;
 } node;
+
+void print_list(node* startpt)
+{
+	node* temp;
+	temp = startpt;
+	printf("%d\n", temp->data);
+	while (temp->data)
+	{
+		temp = temp->next;
+		printf("it broke\n");
+		printf("%d\n", temp->data);
+	}
+}
+
+struct node * insert_front(struct node* pter, int num)
+{
+
+}
+
+struct node * free_list(struct node* pter)
+{
+
+}
