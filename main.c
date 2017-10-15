e3db012101db933432d3825c4414387f80bb78b7
bleung329 <bleung@stuy.edu>
2017-10-15 16:12:20 -0400

    Yikes thats a lot of warnings

--- main.c
+++ main.c
@@ -1,3 +1,8 @@
+/*
+Brian Leung
+PD 10 Systems
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include "node.h"
@@ -6,9 +11,18 @@ int main()
 {
 	node* blah = (node*) calloc(1,sizeof(node));
 	blah->data = 3;
-	printf("%d\n", blah->data);
-	printf("%p\n", blah->next);
+	blah = insert_front(blah, 88);
+	blah = insert_front(blah, 1231);
+	//printf("%d\n", blah->data);
+	//blah->next = (node*) calloc(1,sizeof(node));
+//	brrt->data = 17;
+	//printf("%p\n", blah->next);
+//	printf("%p\n", brrt);
+//	printf("%p\n", blah);
+//	printf("%d\n", sizeof(node));
 	print_list(blah);
+	free_list(blah);
+	printf("Free\n");
 	free(blah);
 	return 0;
 }
--- node.h
+++ node.h
@@ -12,20 +12,31 @@ void print_list(node* startpt)
 	node* temp;
 	temp = startpt;
 	printf("%d\n", temp->data);
-	while (temp->data)
+	while (temp->next)
 	{
 		temp = temp->next;
-		printf("it broke\n");
+		//printf("c machine Broke\n");
 		printf("%d\n", temp->data);
 	}
 }
 
 struct node * insert_front(struct node* pter, int num)
 {
-
+	node* newthing = (node*)calloc(1,sizeof(node));
+	newthing->data = num;
+	newthing->next = pter;
+	pter = newthing;
+	return newthing;
 }
 
 struct node * free_list(struct node* pter)
 {
-
+	node* temp;
+	temp = pter;
+	while (temp->next)
+	{
+		temp = temp->next;
+		free (temp);
+	}
+	return NULL;
 }
